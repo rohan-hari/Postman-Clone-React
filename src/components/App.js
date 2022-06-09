a8a9374daa52d3e0acc0d19b3f965a27f5cff68a
rohanhr3452 <rohanhr3452@gmail.com>
2022-06-09 18:14:12 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <span className="text-4xl">sdf</span>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
