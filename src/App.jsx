95bc4e427a87885971283a827e237815c0f95437
rohanhr3452 <rohanhr3452@gmail.com>
2022-07-08 01:21:00 +0530

    design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Navbar from './components/navbar/Navbar';
 import Request from './components/workspace/Request';
 import Response from './components/workspace/Response';
 
 const App = () => {
   return (
     <>
+    <Navbar />
     <Request />
     <Response />
     </>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='nav-main'>
+        <div className='nav-container'>
+            <a href="/"><span>POSTMAN CLONE</span></a>
+        </div>
+    </div>
+  )
+}
