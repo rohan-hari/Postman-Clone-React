7af78b1d1db04c77472163b084dd4cf82b2f3de0
rohanhr3452 <rohanhr3452@gmail.com>
2022-07-13 14:41:28 +0530

    commit

---
+++ src/components/Inputs/ReqInputs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReqInputs() {
+  return (
+    <div>ReqInputs</div>
+  )
+}
--- src/components/workspace/Request.jsx
+++ src/components/workspace/Request.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import ReqInputs from '../Inputs/ReqInputs'
+
 export default function Request() {
   return (
-    <div className=''>sd</div>
+    <>
+    <ReqInputs />
+    </>
   )
 }
