2066fd9c8494f63bf602fb2c81afaf336c433227
rohanhr3452 <rohanhr3452@gmail.com>
2022-07-30 01:06:25 +0530

    update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Navbar from './components/Navbar/Navbar';
-import Request from './components/Workspace/Request';
-import Response from './components/Workspace/Response';
+import Navbar from './components/navbar/Navbar';
+import Request from './components/workspace/Request';
+import Response from './components/workspace/Response';
 
 const App = () => {
   return (
